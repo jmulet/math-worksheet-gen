895171cfbf27b08757b713879ac1aa41cf02927f
jmulet <pep.mulet@gmail.com>
2018-06-11 08:29:17 +0200

    prova

--- src/math/Equation.ts
+++ src/math/Equation.ts
@@ -1,44 +1,36 @@
-import { Formatter } from '../util/Formatter';
-import { Random } from '../util/Random';
-import { Giac } from './Giac';
-import { Numeric } from './Numeric';
-import { Polynomial } from './Polynomial'; 
+import { Interval } from "./Intervals";
+import { Numeric } from "./Numeric";
+import { Giac } from "./Giac";
+import { Polynomial } from "./Polynomial";
+import { Random } from "../util/Random";
 
 export enum EQUATION_SYMBOLS {
-    EQ = 0,
-    LT = 1,
-    GT = 2,
-    LEQ = 3,
-    GEQ = 4
+    eq  = 0,
+    lt = 1,
+    gt = 2,
+    leq = 3,
+    geq = 4
 }
-
-function stringify(c: Numeric): string {
-    let str = "";
-    if (c.isZero()) {
-        return "";
-    }
-    if (!c.isNegative()) {
-        str = "+";
-    }
-    str += c.toTeX();
-    return str;
-}
-
 
 export class Equation {
+    
+    answers: Interval[];
 
-    answers: any[];
-
-    constructor(public rnd: Random, public bar="x", public lhs?: string, public rhs?, public symbol = EQUATION_SYMBOLS.EQ) {
-
+    constructor(public lhs?: string, public rhs?, public symbol = EQUATION_SYMBOLS.eq) {
     }
+
     // Polynomial equations ==================
-    linealFromRoot(root: Numeric, complexity = 2) {
-        this.polynomialFromRoots([root], 1, complexity);
+
+    linealFromRoot(root: Numeric, complexity = 2){
+        this.polynomialFromRoots(root, 1, complexity);
     }
 
     quadraticFromRoots(roots: Numeric[], complexity = 2) {
         this.polynomialFromRoots(roots, 2, complexity);
+    };
+
+    factorizableFromRoots(roots){
+        
     };
 
     biquadraticFromQuadraticRoots(roots: Numeric[], complexity = 1) {
@@ -50,135 +42,102 @@ export class Equation {
         let B = r1.add(r2).oposite();
         let C = r1.multiply(r2);
         // Get rid of possible denominators
-        const lcm = parseInt(Giac.lcm(B.Re["d"], C.Re["d"]));
+        const lcm = parseInt(Giac.lcm(B["d"], C["d"]));
         if (lcm > 1) {
             A = lcm;
             B = B.multiply(Numeric.fromNumber(lcm));
             C = C.multiply(Numeric.fromNumber(lcm));
         }
-        let Astr = "";
-        if (A !== 1) {
-            Astr = A + "";
-        }
-
-        const Bsrt = stringify(B);
-        let poly = new Polynomial([A, 0, B, 0, C]);
-        if (complexity > 1) {
-            const poly2 = this.rnd.polynomial({ maxDegree: 4 });
-            poly = poly.add(poly2);
-            this.rhs = poly2.toTeX();
-        } else {
-            this.rhs = "0";
-        }
-        this.lhs = poly.toTeX();
-
-
-        this.answers = [];
-        if (!r1.isNegative()) {
-            this.answers.push(-Math.sqrt(r1.toNumber()));
-            this.answers.push(Math.sqrt(r1.toNumber()));
-        }
-        if (!r2.isNegative()) {
-            this.answers.push(-Math.sqrt(r2.toNumber()));
-            this.answers.push(Math.sqrt(r2.toNumber()));
-        }
+        this.lhs = "";
+        this.rhs = "";
     };
 
-    polynomialFromRoots(roots: Numeric[], degree = 2, complexity = 1) {
-        this.answers = roots;
-        let poly = Polynomial.fromRoots(roots);
+    polynomialFromRoots(roots, rnd: Random, degree = 2, complexity = 0) {        
+       
+        const poly1 = Polynomial.fromRoots(roots);
+        this.lhs = poly1.toTeX();
+        this.rhs = "0";
 
-        if (roots.length < degree) {
-            // Create a irreductible polynomial of second degree
-            const b = this.rnd.intBetween(-5, 5);
-            const c = Math.round(b * b / 4 + this.rnd.intBetween(2, 5));
-            poly = poly.multiply(new Polynomial([1, b, c]));
-        }
-        if (complexity === 0) {
-            //assume factorizable
-            this.lhs = poly.toFactorForm();
-            this.rhs = "0";
-        } else if (complexity === 1) {
-            //assume basic = 0
-            this.lhs = poly.toTeX();
-            this.rhs = "0";
-        } else if (complexity > 1) {
-            if (roots.length === 2 && degree === 2) {
-                const [r1, r2] = roots;
-                const beta = r1.substract(r2).divide(Numeric.fromNumber(2)).power(2);
-                const alpha = r1.add(r2).divide(Numeric.fromNumber(2));
-                const poly3 = new Polynomial([1, alpha.oposite()]);
-                this.lhs = "\\left(" + poly3.toTeX(this.bar) + "\\right)^2";
-                this.rhs = beta.toTeX();
-            } else {
-                const poly2 = this.rnd.polynomial({ minDegree: 1, maxDegree: 1 });
-                const poly3 = poly2.power(2);
-                this.lhs = poly.add(poly3).toTeX(this.bar);
-                this.rhs = "\\left(" + poly2.toTeX(this.bar) + "\\right)^2";
+        if (degree === 1) {
+            switch(complexity) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;    
+            }
+        } else if (degree === 2) {
+            switch(complexity) {
+                case 0:
+                    const poly1 = Polynomial.fromRoots(roots);
+                    this.lhs = poly1.toTeX();
+                    this.rhs = "0";
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;    
+            }
+        } else if (degree === 3) {
+            switch(complexity) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;    
+            }
+        } else if (degree ===4) {
+            switch(complexity) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;    
+            }
+        } else {
+            let poly2;
+            switch(complexity) {
+                case 0:                   
+                    this.lhs = poly1.toFactorForm();
+                    this.rhs = "0";
+                    break;
+                case 1:
+                    this.lhs = poly1.toTeX();
+                    this.rhs = "0";            
+                    break;
+                case 2:
+                    poly2 = rnd.polynomial({maxDegree: degree, domain: 'Z'});
+                    this.lhs = poly1.add(poly2).toTeX();
+                    this.rhs = poly2.toTeX();            
+                    break; 
+                case 3:
+                    poly2 = rnd.polynomial({maxDegree: degree, domain: 'Q'});
+                    this.lhs = poly1.add(poly2).toTeX();
+                    this.rhs = poly2.toTeX();    
+                    break;  
+                case 4:
+                    poly2 = rnd.polynomial({maxDegree: degree, domain: 'Q'});
+                    this.lhs = poly1.add(poly2).toTeX();
+                    this.rhs = poly2.toTeX();    
+                    break;    
             }
         }
     };
 
     // Rational equations =====================
-    rational(r = 10, complexity = 1) {
 
-    }
-
-    // irrational equations ======================
-    irrational(r = 10, complexity = 1) {
-        if (complexity <= 1) {
-            const cas = this.rnd.intBetween(0, 1);
-            const a = this.rnd.intBetweenNotZero(-r, r);
-            const b = this.rnd.intBetweenNotZero(-r, r);
-            const c = this.rnd.intBetweenNotZero(-r, r);
-            if (cas === 0) {
-                this.lhs = "\\sqrt{" + Formatter.numericXstringTeX(false, Numeric.fromNumber(a), this.bar) + Formatter.append(b) + "} ";
-                this.rhs = c + "";
-                const num = c * c - b;
-                this.answers = []
-                if (c >= 0) {
-                    const root = Numeric.fromFraction(num, a);
-                    this.answers.push(root);
-                }
-            } else {
-                const sigma = 2 * this.rnd.intBetween(2, 5) - 1;  //random odd number
-                const delta = Numeric.fromFraction(sigma * sigma - 1, 4);
-                const b = Numeric.fromNumber(a).add(delta);
-                this.lhs = "\\sqrt{" + this.bar + Formatter.append(-a) + "} ";
-                this.rhs = this.bar + Formatter.append(b.oposite());
-                const root = Numeric.fromFraction(1 + sigma, 2).add(b);
-                this.answers = [root]
-            }
-        } else if (complexity >= 2) {
-            const cas = this.rnd.intBetween(0, 2);
-
-            let a = this.rnd.intBetweenNotZero(-r, r);
-            let b = this.rnd.intBetweenNotZero(-r, r);
-            let c = this.rnd.intBetweenNotZero(-r, r);
-            if (cas === 0) {
-                this.lhs = "\\sqrt{" + this.bar + Formatter.append(-a) + "} " + Formatter.append(b);
-                this.rhs = "\\sqrt{" + this.bar + Formatter.append(-c) + "}";
-                const num = Math.pow(c + b * b - a, 2);
-                const root = Numeric.fromNumber(a).add(Numeric.fromFraction(num, 4 * b * b));
-                this.answers = [root];
-            } else if (cas === 1) {
-                c = Math.abs(c);
-                this.lhs = "\\sqrt{("+ this.bar + Formatter.append(-a) + ")^2 + (" + this.bar + Formatter.append(-b) + ")^2} ";
-                this.rhs = c + "";
-                const plainEqn = "sqrt(x-("+a+")**2 + (x-("+b+"))**2) = " + c;
-                this.answers = [Giac.evaluate("latex(solve(" + plainEqn + "))").replace(/"/g, "").replace(/,/g, ";")];
-            } else if (cas === 2) {
-                a = this.rnd.intBetween(-r, -1);
-                b = a + this.rnd.intBetween(-r, -1);
-                c = Math.round(Math.sqrt(a - b)) + this.rnd.intBetween(1, r);
-                this.lhs = "\\sqrt{" + this.bar + Formatter.append(-a) + "} " + " + \\sqrt{" + this.bar + Formatter.append(-b) + "} ";
-                this.rhs = c + "";
-                const num = a * a - 2 * a * b + 2 * a * c * c + b * b + 2 * b * c * c + c * c * c * c;
-                const root = Numeric.fromFraction(num, 4 * c * c);
-                this.answers = [root]
-            }
-        }
-    }
+    // Radical equations ======================
 
     // Exponential equations ==================
 
@@ -188,24 +147,26 @@ export class Equation {
 
 
     toString() {
-        return this.lhs + " = " + this.rhs;
+        return this.lhs + " " + this.symbol + " " + this.rhs;
     }
 
     toTeX() {
-        return this.lhs + " =" + this.rhs;
+        return this.lhs + " " + this.symbol + " " + this.rhs;
+    }
+
+    solutions(): Interval[] {
+        if (this.symbol === EQUATION_SYMBOLS.eq)  {
+            // Equation
+
+        } else {
+            // Innequation
+
+        }
+        return this.answers;
     }
 
     solutionsTeX(): string {
-        if ((this.answers || []).length === 0) {
-            return "Cap solució";
-        }
-        return this.answers.map(e => {
-            let e2: any = e;
-            if (e2.toTeX) {
-                e2 = e2.toTeX();
-            }
-            return "$x=" + e2 + "$";
-        }).filter((item, pos, self) => self.indexOf(item) == pos).join("; ");
+        return "todo";
     }
 
 }
